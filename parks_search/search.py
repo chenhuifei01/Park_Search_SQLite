8aee1b00b3e65b98f0e83da9c20f7d93673dc9ec
Chen Hui Wang <chenhui1@linux2.cs.uchicago.edu>
2023-02-17 20:32:38 -0600

    descriptive variables

--- parks_search/search.py
+++ parks_search/search.py
@@ -42,39 +42,38 @@ def search_parks(params):
     """
     # TODO: implement this function
 
-    # Start by opening a connection to the database and registering the haversine_distance
-    # function with the connection. This will allow you to use the haversine_distance function
-    # in your SQL queries as shown in the README.
-
+    # opening a connection to the database and registering the haversine_distance
     connection = sqlite3.connect('data/parks.db')
     connection.create_function('haversine_distance', 4, haversine_distance)
     connection.row_factory = sqlite3.Row        
     cursor = connection.cursor()
 
+    # track the query objects
     answer = []
     querylist = []
 
-
     select_query = 'SELECT'
-    # from_query = ' FROM parks as p JOIN park_times as t '
     where_query = ' WHERE '
 
+    # confirm the select query 
+    # 1. haversine_distance 
     if 'near' in params:
         select_query += ' haversine_distance(?, ?, latitude, longitude) AS distance, p.name, p.address, p.description, p.history, p.url '
         querylist.extend([params['near'][0], params['near'][1]])
     else:
         select_query += ' p.name, p.address, p.description, p.history, p.url '
-
+    # 2. join two tables or not
     if 'open_at' in params:
         select_query += ', t.day, t.open_time, t.close_time FROM parks as p JOIN park_times as t ON p.id = t.park_id' 
     else:
         select_query += 'FROM parks as p'
     
+    # run through params
     for k, v in params.items():
         if k == 'name':
             where_query += ' name LIKE ? AND '
-            v = v + '%'
-            querylist.append(v)
+            name = v + '%'
+            querylist.append(name)
         
         elif k == 'query_terms':
             for token in v:
@@ -84,29 +83,29 @@ def search_parks(params):
 
         elif k == 'zip_code':
             where_query += ' address LIKE ? AND '
-            v = '%' + v + '%'
-            querylist.append(v)
+            zipcode = '%' + v + '%'
+            querylist.append(zipcode)
 
         elif k == 'open_at':
             where_query += ' day = ? AND open_time <= ? AND close_time >= ? AND '
-            querylist.extend([v[0], v[1], v[1]])
+            day = v[0]
+            open_time = v[1]
+            close_time = v[1]
+            querylist.extend([day, open_time, close_time])
         
         elif k == 'near':
             where_query += ' distance <= ? AND '
-            querylist.append(v[2])
+            distance = v[2]
+            querylist.append(distance)
             
-
+    # intergrate the queries
     myquery = select_query + where_query 
+    # get rid of the last " AND "
     myquery = myquery[ : -5]
 
-
+    # execute and fetchall
     cursor.execute(myquery, querylist)
     for row in cursor.fetchall():
         answer.append(row)
-    
 
     return answer
-  
-
-    
-    
